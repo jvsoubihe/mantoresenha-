ad8957d3ac1d4604d13ab91e2ff8c9296b0ceceb
jvsoubihe <jvsoubihe@gmail.com>
2026-09-22 20:23:20 -0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,1 +1,2 @@
-window.MR_SUPABASE = { url: '', anonKey: '' };
+window.MR_SUPABASE = { url: 'https://ohmazwsbwtpsmwxpuuot.supabase.co', anonKey: 'sb_publishable_fTmsUV5Yv0kNyyk27_Blww_hZuPB62K
+' };
